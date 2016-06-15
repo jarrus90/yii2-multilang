8f8ff4c903982ff1e437e9e58d63b65da57b06fd
Vladimir Stovbchatij <unreality90@mail.ru>
2016-06-15 13:44:28 +0300

    Added listmap method

---
+++ Controllers/ChangeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace jarrus90\Multilang\Controllers;
+
+use Yii;
+use jarrus90\Core\Web\Controllers\FrontController as Controller;
+
+class ChangeController extends Controller {
+    public function actionSet($lang) {
+        
+    }
+}
--- Models/Language.php
+++ Models/Language.php
@@ -2,7 +2,9 @@
 
 namespace jarrus90\Multilang\Models;
 
+use Yii;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 use jarrus90\Content\traits\ModuleTrait;
 
 class Language extends ActiveRecord {
@@ -12,5 +14,11 @@ class Language extends ActiveRecord {
     /** @inheritdoc */
     public static function tableName() {
         return '{{%languages}}';
+    }
+    
+    public static function listMap(){
+        return ArrayHelper::map(static::find()->asArray()->all(), 'code', function($model){
+            return $model['name'] . ($model['enabled'] ? '' : ' (' . Yii::t('multilang', 'disabled') .')');
+        });
     }
 }
