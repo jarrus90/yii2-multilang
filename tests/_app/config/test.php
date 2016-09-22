96740af6ce1379b33483ffc1bacdfee21ef6f1de
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-22 12:04:49 +0300

    Tests inited

---
+++ tests/_app/config/test.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'id' => 'yii2-user-tests',
+    'basePath' => dirname(__DIR__),
+    'language' => 'en-US',
+    'aliases' => [
+        '@jarrus90/Multilang' => dirname(dirname(dirname(__DIR__))),
+        '@tests' => dirname(dirname(__DIR__)),
+        '@vendor' => VENDOR_DIR,
+        '@bower' => VENDOR_DIR . '/bower-asset',
+    ],
+    'bootstrap' => ['jarrus90\Multilang\Bootstrap'],
+    'modules' => [
+        'multilang' => [
+            'class' => 'jarrus90\Multilang\Module'
+        ],
+    ],
+    'components' => [
+        'db' => require __DIR__ . '/db.php',
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'showScriptName' => true,
+        ],
+        'request' => [
+            'cookieValidationKey' => 'test',
+            'enableCsrfValidation' => false,
+        ],
+    ],
+    'params' => [],
+];
