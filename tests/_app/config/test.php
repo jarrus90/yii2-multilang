7a85db7b6c68f5d8b2bac7f11b44fe9e91e59de7
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-23 14:04:55 +0300

    Fix bootstrapping for tests

--- tests/_app/config/test.php
+++ tests/_app/config/test.php
@@ -1,23 +1,9 @@
 <?php
 
-return [
+return \yii\helpers\ArrayHelper::merge(require_once __DIR__ . '/common.php', [
     'id' => 'yii2-multilang-tests',
-    'basePath' => dirname(__DIR__),
-    'language' => 'en-US',
     'aliases' => [
-        '@jarrus90/Multilang' => dirname(dirname(dirname(__DIR__))),
-        '@tests' => dirname(dirname(__DIR__)),
-        '@vendor' => VENDOR_DIR,
         '@bower' => VENDOR_DIR . '/bower-asset',
-    ],
-    'bootstrap' => ['jarrus90\Multilang\Bootstrap'],
-    'modules' => [
-        'user' => [
-            'class' => 'jarrus90\User\Module'
-        ],
-        'multilang' => [
-            'class' => 'jarrus90\Multilang\Module'
-        ],
     ],
     'components' => [
         'db' => require __DIR__ . '/db.php',
@@ -33,4 +19,4 @@ return [
         ],
     ],
     'params' => [],
-];
+]);
