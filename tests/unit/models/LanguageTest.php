d00b03ca546be34c75e2b9f4dd4ec5304801a6f3
Vladimir Stovbchatij <unreality90@mail.ru>
2017-01-26 13:36:08 +0200

    Finished model tests

--- tests/unit/models/LanguageTest.php
+++ tests/unit/models/LanguageTest.php
@@ -11,36 +11,30 @@ class LanguageTest extends \yii\codeception\TestCase {
         ];
     }
 
-    public function testFindMethod() {
+    public function testFind() {
         $this->assertNotEmpty(Language::findOne(['code' => 't1']), 'Find one item.');
         $this->assertArrayHasKey('t2', Language::listMap());
         $this->assertArrayNotHasKey('t2', Language::listMap(true));
     }
 
-    public function testSearchAll() {
+    public function testSearch() {
         $item = new Language();
         $item->scenario = 'search';
         $result = $item->search([]);
         $this->assertEquals($result->totalCount, 2);
-    }
 
-    public function testSearchCode() {
         $itemCode = new Language();
         $itemCode->scenario = 'search';
         $resultCode = $itemCode->search([$itemCode->formName() => ['code' => 't1']]);
         $this->assertEquals(1, $resultCode->totalCount, 'Count of search results by code');
         $this->assertEquals('t1', $resultCode->models[0]->code);
-    }
 
-    public function testSearchName() {
         $itemName = new Language();
         $itemName->scenario = 'search';
         $resultName = $itemName->search([$itemName->formName() => ['name' => 'Test 1']]);
         $this->assertEquals(1, $resultName->totalCount, 'Count of search results by name');
         $this->assertEquals('Test 1', $resultName->models[0]->name);
-    }
 
-    public function testSearchActive() {
         $itemActive = new Language();
         $itemActive->scenario = 'search';
         $resultActive = $itemActive->search([$itemActive->formName() => ['is_active' => false]]);
@@ -61,20 +55,62 @@ class LanguageTest extends \yii\codeception\TestCase {
 
         $itemInvalid = new Language();
         $itemInvalid->scenario = 'create';
-        $this->assertEquals(false, $itemInvalid->validate(), 'Validate failed');
+        $itemInvalid->setAttributes([
+            'code' => NULL,
+            'name' => NULL,
+            'flag' => NULL,
+            'is_active' => NULL,
+        ], false);
+        $this->assertEquals(false, $itemInvalid->save(), 'Invalid save');
         $this->assertArrayHasKey('code', $itemInvalid->errors, 'Empty code input');
         $this->assertArrayHasKey('name', $itemInvalid->errors, 'Empty name input');
         $this->assertArrayHasKey('flag', $itemInvalid->errors, 'Empty flag input');
         $this->assertArrayHasKey('is_active', $itemInvalid->errors, 'Empty is_active input');
+    }
 
-        $itemInvalidCode = new Language();
-        $itemInvalidCode->scenario = 'create';
-        $itemValid->setAttributes([
+    public function testUpdate() {
+        $itemValid = Language::findOne(['code' => 't1']);
+        $itemValid->scenario = 'update';
+        $itemValid->setAttributes($itemValid->getAttributes([
+            'name', 'flag', 'is_active'
+        ]), false);
+        $this->assertEquals(true, $itemValid->save(), 'Valid save');
+
+
+        $itemInvalid = Language::findOne(['code' => 't1']);
+        $itemInvalid->scenario = 'update';
+        $itemInvalid->setAttributes([
+            'name' => NULL,
+            'flag' => NULL,
+            'is_active' => NULL,
+        ], false);
+        $this->assertEquals(false, $itemInvalid->save(), 'Invalid save');
+        $this->assertArrayHasKey('name', $itemInvalid->errors, 'Empty name input');
+        $this->assertArrayHasKey('flag', $itemInvalid->errors, 'Empty flag input');
+        $this->assertArrayHasKey('is_active', $itemInvalid->errors, 'Empty is_active input');
+    }
+    
+    public function testDelete() {
+        $item = Language::find()->one();
+        $this->assertEquals(true, $item->delete(), 'Delte item');
+    }
+
+    public function testCodeValidator() {
+        $newInvalid = new Language();
+        $newInvalid->scenario = 'create';
+        $newInvalid->setAttributes([
             'code' => 't1'
         ], false);
-        $itemInvalidCode->validate();
-        $this->assertArrayHasKey('code', $itemInvalidCode->errors, 'Not equal code');
+        $newInvalid->validate();
+        $this->assertArrayHasKey('code', $newInvalid->errors, 'New item duplicate code');
 
+        $itemInvalid = Language::findOne(['code' => 't1']);
+        $itemInvalid->scenario = 'update';
+        $newInvalid->setAttributes([
+            'code' => 't2'
+        ], false);
+        $newInvalid->validate();
+        $this->assertArrayHasKey('code', $newInvalid->errors, 'Old item duplicate code');
     }
 
 }
